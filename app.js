851f57189c7f8e78b6f8f2b47232ba23f14877be
kamon <kannon0205@gmail.com>
2020-10-28 01:27:43 +0900

    テスト

--- app.js
+++ app.js
@@ -854,11 +854,12 @@ app.get('/', authMiddleware, (req, res) => {
   });
 });
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  response.end('Hello, World\n');
-}).listen(process.env.PORT, process.env.IP);
+//http.createServer(function (request, response) {
+//  response.writeHead(200, {
+//    'Content-Type': 'text/plain'
+//  });
+//  response.end('Hello, World\n');
+//}).listen(process.env.PORT, process.env.IP);
 
+app.listen(process.env.PORT, process.env.IP);
 //app.listen(3000);
